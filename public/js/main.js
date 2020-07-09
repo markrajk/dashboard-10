0abe637e7027e8bf3046bb53e94d40e81e41b8fd
markrajk <marko88rajkovic@gmail.com>
2020-07-09 09:19:38 +0200

    compare page full width body & related changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,19 @@
+var sidenav = document.querySelector('.sidebar');
 var sidenavMenu = document.querySelector('.sidebar-menu');
 var sidenavMenuToggle = document.querySelector('.sidebar-menu-toggle');
 var sidenavMenuToggleIcon = document.querySelector('.sidebar-menu-toggle i');
 
 sidenavMenuToggle.addEventListener('click', function () {
-  sidenavMenu.classList.toggle('sidebar-menu-close-anim');
+  sidenav.classList.toggle('sidebar-menu-close-anim');
   sidenavMenuToggleIcon.classList.toggle('sidebar-menu-close-icon-rotate');
 });
 
 function closeMenuOnSmall() {
   if (window.innerWidth < 1320) {
-    sidenavMenu.classList.add('sidebar-menu-close-anim');
+    sidenav.classList.add('sidebar-menu-close-anim');
     sidenavMenuToggleIcon.classList.add('sidebar-menu-close-icon-rotate');
   } else {
-    sidenavMenu.classList.remove('sidebar-menu-close-anim');
+    sidenav.classList.remove('sidebar-menu-close-anim');
     sidenavMenuToggleIcon.classList.remove('sidebar-menu-close-icon-rotate');
   }
 }
